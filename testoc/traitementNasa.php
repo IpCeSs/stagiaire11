8a720140653fd9f8c1643af359af6486c809ec45
Cecilia <ceciliapigeolet@gmail.com>
2018-01-31 12:59:37 +0100

    NasaFinal

--- testoc/traitementNasa.php
+++ testoc/traitementNasa.php
@@ -2,16 +2,14 @@
 // On définie la variable mot de passe imposée dans le TD
 $password='kangourou';
 //on vérifie que les champs ne soient pas vides soit différents =! de '' chaine vide
+//on aurait pu utiliser la fonction empty()
 
 if ($_POST['nom']!=''&& $_POST['pw']!='') {
-    
-    if($_POST['pw']===$password){
-        echo 'You successfully entered The NASA Network!</> Be ready to conquer the world!!!';
+    if ($_POST['pw']===$password) {
+        echo 'You successfully entered The NASA Network!</br> The secret Code is BANANA </br>Be ready to conquer the world!!!';
+    } else {
+        echo 'The password doesn\'t match, NASA SWAT team is on its way';
     }
-    else {
-       echo 'The password doesn\'t match, NASA SWAT team is on its way';
-    }
-
 } else {
     echo 'WARNING you didn\'t fill all the blank fields.</br> The NASA police is coming for you';
 }
