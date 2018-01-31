4f787a06f1f23002416ee25cc0f8ec72549fefa3
Cecilia <ceciliapigeolet@gmail.com>
2018-01-31 10:28:54 +0100

    debut td OC

---
+++ testoc/formulaireNasa.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Accéder au serveur de la Nasa</title>
+</head>
+<body>
+    <h1 color="blue">REACH THE STARS </h1>
+    <h2> You're about to access the NASA NETWORK</h2></br>
+    <form method="post" action="traitementNasa">
+    Name <input type="text" name="nom"></br>
+    Password <input type="text" name="pw"></br>
+    <input type="submit" value="Access NASA Network">
+</body>
+</html>
