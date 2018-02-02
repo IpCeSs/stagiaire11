f83661a6a74f7b3702df7fb99992a329891cd786
Cecilia <ceciliapigeolet@gmail.com>
2018-02-02 14:59:52 +0100

    modif1 blog

--- BD/blog.php
+++ BD/blog.php
@@ -1,12 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link rel='stylesheet' href='blog.css'>
+    <title>TD BLOG</title>
 </head>
+
 <body>
-    
+    <h1 style='color:#F01951' align='center'>Le BlOg TrOp BiEn !</h1>
+    <h3>Titre Post 1</h3>
+    <div class="news">
+        <p>Contenu Post</p>
+    </div>
+   <div class="comment"> <a href="">Commentaire</a> </div>
 </body>
+
 </html>
--- BD/blogTraitement.php
+++ BD/blogTraitement.php
@@ -1,1 +1,7 @@
 <? header('location:blog.php');
+try {
+    $blog=new PDO('mysql:host=stagiaireonze;dbname=tdblog;charset=utf8', 'root', '');
+}catch (Exception $e){
+die('erreur:'.$e->getMessage());
+}
+
