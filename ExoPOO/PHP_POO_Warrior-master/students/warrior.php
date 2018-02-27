aa4b26f72ee9237b959e5aca2799213c0641d31e
Cecilia <ceciliapigeolet@gmail.com>
2018-02-27 10:54:01 +0100

    première partie jusqu'à point 15 OK

--- ExoPOO/PHP_POO_Warrior-master/students/warrior.php
+++ ExoPOO/PHP_POO_Warrior-master/students/warrior.php
@@ -3,9 +3,94 @@
 require_once __DIR__ . "/../base/localWarrior.php";
 
 
-$GLOBALS['warriorID'] = 'azertyuiop';
+$GLOBALS['warriorID'] = 'cess';
 
 // Définissez vos class de combattants
-abstract class Warrior extends LocalWarrior {
+ abstract class Warrior extends LocalWarrior {
+    public $id;
+    public $name;
+    public $speed;
+    public $life;
+    public $shield;
+    public $weapon;
+    public $imageUrl;
 
+    public function __construct($id,$speed=30,$life=100,$shield=20){
+        $this->id=$id;
+        $this->speed=$speed;
+        $this->life=$life;
+        $this->shield=$shield;
+
+    }
+
+    public function SetWeapon($weapon){
+        $this->weapon=$weapon;
+    }
+
+    public function setImageUrl($imageUrl){
+        $this->imageUrl=$imageUrl;
+    }
 };
+////////////////////////////////////////////////
+class StartrekWarrior extends Warrior{
+public $mentalPower;
+    public function power(){
+        return $this->mentalPower;
+    }
+    public function __construct($id,$speed=30,$life=100,$shield=20,$mentalPower=8){
+        parent::__construct($id,$speed,$life,$shield);
+$this->mentalPower=$mentalPower;
+    }
+
+
+}
+////////////////////////////////////////////////
+class MarvelWarrior extends Warrior{
+
+    public $superPower;
+
+    public function power(){
+        return $this->superPower;
+    }
+    public function __construct($id,$speed=30,$life=100,$shield=20,$superpower=100){
+        parent::__construct($id,$speed,$life,$shield);
+        $this->superPower=$superpower;
+
+}
+
+}
+//////////////////////////////////////
+class PokemonWarrior extends Warrior{
+    public $level;
+    public function power(){
+        return $this->level;
+
+    }
+    public function __construct($id,$speed=30,$life=100,$shield=20,$level=1){
+        parent::__construct($id,$speed,$life,$shield);
+        $this->level=$level;
+
+
+    }
+
+
+}
+
+////
+class Weapon{
+    public $id;
+    public $strength;
+    public $imageUrl;
+
+    public function __construct($id,$strength)
+    {
+        $this->id=$id;
+        $this->strength=$strength;
+    }
+
+    public function setImageUrl($imageUrl){
+        $this->imageUrl=$imageUrl;
+    }
+}
+
+
